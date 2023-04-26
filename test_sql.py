a1f04a9999a2c3c0fb504b9e032517442d459d11
Cheng Dobbs <dobbs.ch26@gmail.com>
2023-04-26 23:26:05 +0800

    update test_sql

--- test_sql.py
+++ test_sql.py
@@ -2,18 +2,18 @@ import pymysql,datetime
 host = "127.0.0.1"
 port = 3306
 user = "root"
-passwd = "4b4f8a297c377df7"
+passwd = "00000123"
 db = pymysql.Connect(host=host, port=port, user=user, passwd=passwd)
 cur = db.cursor()
 cur.execute(f'''show databases;''')
 print(cur.fetchall())
 cur.execute(f'''use qset_SPJ_MNG;''')
 print(cur.fetchall())
-cur.execute(f'''select jname from j where jno in ( select distinct jno from spj where sno in ( select sno from s where city='上海' ) order by jno );''')
+cur.execute(f'''select jname from J where jno in ( select distinct jno from SPJ where sno in ( select sno from S where city='上海' ) order by jno );''')
 a = sorted(cur.fetchall())
 # print(sorted(cur.fetchall()))
 print(a)
-cur.execute(f'''select distinct jname from j where exists(select * from spj,s where spj.jno = j.jno and spj.sno = s.sno and s.city = '上海');''')
+cur.execute(f'''select distinct jname from J where exists(select * from SPJ,S where SPJ.jno = J.jno and SPJ.sno = S.sno and S.city = '上海');''')
 # print(sorted(cur.fetchall()))
 b = sorted(cur.fetchall())
 # print(sorted(cur.fetchall()))
